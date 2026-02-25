e7270668e555012cd652c70fc3be1f91a0c61a44
bethkimani <kbwaceke@gmail.com>
2026-02-25 14:29:33 +0300

    Defined API routes for users

---
+++ backend/routes/api.php
@@ -0,0 +1,9 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\WalletController;
+use App\Http\Controllers\TransactionController;
+
+Route::apiResource('users', UserController::class);
+Route::apiResource('users.wallets', WalletController::class)->shallow();
+Route::apiResource('wallets.transactions', TransactionController::class)->shallow();
