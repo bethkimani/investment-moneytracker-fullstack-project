00489a4809a0be50e027e83456d2c97535f17edf
bethkimani <kbwaceke@gmail.com>
2026-02-25 14:11:55 +0300

    Updated wallets and transactions migrations with relationships

--- backend/database/migrations/2026_02_25_110741_create_transactions_table.php
+++ backend/database/migrations/2026_02_25_110741_create_transactions_table.php
@@ -1,26 +1,23 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('wallet_id')->constrained()->onDelete('cascade');
+            $table->enum('type', ['income', 'expense']);
+            $table->decimal('amount', 10, 2);
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('transactions');
     }
