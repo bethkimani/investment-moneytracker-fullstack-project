8d86bb2790ceaa5ac4adf771be275b1abd43c0fe
bethkimani <kbwaceke@gmail.com>
2026-02-25 14:16:29 +0300

    Added balance calculation methods to Wallet model

--- backend/app/Models/Transaction.php
+++ backend/app/Models/Transaction.php
@@ -1,10 +1,21 @@
 <?php
-
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['wallet_id', 'type', 'amount', 'description'];
+
+    protected $casts = [
+        'amount' => 'decimal:2',
+    ];
+
+    public function wallet()
+    {
+        return $this->belongsTo(Wallet::class);
+    }
 }
